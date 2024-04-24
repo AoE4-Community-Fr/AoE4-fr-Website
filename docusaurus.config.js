de0c1f642be4ff628641287d840b55763d9992e9
Quentin Horgues <quentin.horgues@outlook.fr>
2024-04-24 19:14:55 +0200

    ajout image lors d'un partage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/AoE_share_card.jpg',
       navbar: {
         title: 'AoE IV : France',
         logo: {
