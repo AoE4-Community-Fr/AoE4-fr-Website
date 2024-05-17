cd6ceb6d502a443a9dbe1f102fddb3b074260166
Quentin Horgues <quentin.horgues@outlook.fr>
2024-05-17 12:04:16 +0200

    ajout lien edition

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,13 +43,13 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/AoE4-Community-Fr/AoE4-fr-Website/blob/master',
+          editUrl: 'https://github.com/AoE4-Community-Fr/AoE4-fr-Website/blob/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/AoE4-Community-Fr/AoE4-fr-Website/blob/master/',
         },
         theme: {
           customCss: './src/css/custom.css',
