70b336f46c9c4b1877499ba108bdf35c21256dcc
Quentin Horgues <quentin.horgues@outlook.fr>
2024-07-23 07:42:48 +0200

    Merge branch 'master' into feature_guide

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,35 +5,15 @@ import Link from '@docusaurus/Link';
 
 const NewsList = [
   {
-    title: 'Master of Realm',
-    img: '/img/organisations/hag/MOR_WEEKEND_2.png',
+    title: 'Le patch note de la saison 8',
+    img: 'https://cdn.ageofempires.com/aoe/wp-content/uploads/2024/07/S8_Update_Banner-1-1080x608.webp',
     description: (
       <>
-        Retrouvez les qualifacations pour le tournoi S-tier casté en français par HAG
+        Retrouvez toutes les nouveautés de la saison 8!
       </>
     ),
-    url: '/event/master-of-realms',
-  },
-  {
-    title: 'Nouveau patch',
-    img: 'https://cdn.ageofempires.com/aoe/wp-content/uploads/2024/03/S7-Patch-1920x1080-1-1080x608.webp',
-    description: (
-      <>
-        Un nouveau correctif est sorti résolvant des bugs et des correction d'équilibrage.
-      </>
-    ),
-    url: '/patch/patch-10.1.48',
-  },
-  {
-    title: 'Mise à jour de la saison 7',
-    img: 'https://cdn.ageofempires.com/aoe/wp-content/uploads/2024/03/S7-Spring-Tournies-Update-1920x1080-1-1080x608.webp',
-    description: (
-      <>
-        Le patch de la saison 7 est sorti avec du nouveau contenu et des changements d'équilibrage
-      </>
-    ),
-    url: '/patch/update-10.0.576',
-  },
+    url: '/patch/update-11.0.782',
+  }
 ];
 
 function News({ img, title, description, url }) {
