08fa94ef4bd8428a25dbc0dde51a4c6a33e84fa1
Quentin Horgues <quentin.horgues@outlook.fr>
2024-08-28 14:06:18 +0200

    corection class react

--- src/components/ButtonImageBox.js
+++ src/components/ButtonImageBox.js
@@ -1,8 +1,8 @@
 
 export default function ButtonImageBox({children}) {
   return (
-    <article class="margin-top--lg">
-      <section class="list-creator row list_node_modules-@docusaurus-theme-classic-lib-theme-DocCategoryGeneratedIndexPage-styles-module">
+    <article className="margin-top--lg">
+      <section className="list-creator row list_node_modules-@docusaurus-theme-classic-lib-theme-DocCategoryGeneratedIndexPage-styles-module">
         {children}
       </section>
     </article>
--- src/components/ImageButton.js
+++ src/components/ImageButton.js
@@ -1,15 +1,15 @@
 export default function ImageButton ({ logo, name, description, link }) {
 return (
-  <article class="colonne-img-button img-button-col col col--6 margin-bottom--lg">
-      <a class="card cardContainer_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module card-img-button"
+  <article className="colonne-img-button img-button-col col col--6 margin-bottom--lg">
+      <a className="card cardContainer_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module card-img-button"
           href={link}>
           <img decoding="async" loading="lazy" src={logo} alt='logo' className="icone-img-button img_ev3q"></img>
           <div className="text-img-button">
-              <h2 class="text--truncate cardTitle_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
+              <h2 className="text--truncate cardTitle_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
                   title={name}>
                   {name}
               </h2>
-              <p class="text--truncate cardDescription_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
+              <p className="text--truncate cardDescription_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
                   title={description}>
                   {description}
               </p>
