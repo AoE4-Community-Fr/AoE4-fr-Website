7aa084d4fe4d5b8550aa29849053afc5b94991cb
Quentin Horgues <quentin.horgues@outlook.fr>
2024-09-08 01:34:15 +0200

    correction lien interne

--- src/components/ImageButton/index.js
+++ src/components/ImageButton/index.js
@@ -2,22 +2,23 @@ import clsx from 'clsx';
 import Link from "@docusaurus/Link";
 import styles from './styles.module.css';
 
-export default function ImageButton ({ logo, name, description, link }) {
-return (
-  <article className={clsx(styles.imgButtonCol,"col col--6 margin-bottom--lg")}>
-      <Link className={clsx(styles.cardImgButton, "card cardContainer_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module")}
-          to={link}>
-          <img decoding="async" loading="lazy" src={logo} alt='logo' className={clsx(styles.iconeImgButton, "img_ev3q")}></img>
-          <div className={styles.textImgButton}>
-              <h2 className="text--truncate cardTitle_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
-                  title={name}>
-                  {name}
-              </h2>
-              <p className="text--truncate cardDescription_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
-                  title={description}>
-                  {description}
-              </p>
-          </div>
-      </Link>
-  </article>
-)};
+export default function ImageButton({ logo, name, description, link }) {
+    return (
+        <article className={clsx(styles.imgButtonCol, "col col--6 margin-bottom--lg")}>
+            <Link className={clsx(styles.cardImgButton, "card cardContainer_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module")}
+                to={link}>
+                <img decoding="async" loading="lazy" src={logo} alt='logo' className={clsx(styles.iconeImgButton, "img_ev3q")}></img>
+                <div className={styles.textImgButton}>
+                    <h2 className="text--truncate cardTitle_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
+                        title={name}>
+                        {name}
+                    </h2>
+                    <p className="text--truncate cardDescription_node_modules-@docusaurus-theme-classic-lib-theme-DocCard-styles-module"
+                        title={description}>
+                        {description}
+                    </p>
+                </div>
+            </Link>
+        </article>
+    )
+};
