bfe9baa32c50dd9879c5e9a13060fe98b19af3e0
qhorgues <quentin.horgues@outlook.fr>
2025-08-03 00:23:05 +0200

    remove error on url failed

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
     organizationName: "AoE4-Community-Fr", // Usually your GitHub org/user name.
     projectName: "AoE4-fr-Website", // Usually your repo name.
 
-    onBrokenLinks: "throw",
+    onBrokenLinks: "warn",
     onBrokenMarkdownLinks: "warn", // 'warn',
 
     // Even if you don't use internationalization, you can use this field to set
